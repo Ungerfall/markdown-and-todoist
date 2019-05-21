0df9c04a40e513c1e780d16ed076decf26bd2502
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-05-21 18:54:37 +0400

    Add TodoistTask model - represents basic properties of todoist task

---
+++ src/MarkdownToTodoist.Core/Model/TodoistTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarkdownToTodoist.Core.Model
+{
+	public class TodoistTask
+	{
+		public string Description { get; set; }
+		public string Project { get; set; }
+		public DateTime DueTo { get; set; }
+		public DateTime? Reminder { get; set; }
+		public IEnumerable<TodoistTask> SubTasks { get; set; }
+	}
+}
