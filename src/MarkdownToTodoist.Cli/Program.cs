55622170898602228c1753b62297e0442d9bfaee
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-05-11 21:15:17 +0400

    Add read from args file

--- src/MarkdownToTodoist.Cli/Program.cs
+++ src/MarkdownToTodoist.Cli/Program.cs
@@ -2,21 +2,27 @@
 using Antlr4.Runtime.Tree;
 using MarkdownToTodoist.Parser;
 using System;
+using System.IO;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace MarkdownToTodoist.Cli
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string input = @"# ANLTR4 event %2021-05-12T18:00:00%
-1. Create presentation %2021-04-14T23:59:59%
-2. Create an abstract %2021-04-21T23:59:59%";
+            if (args == null || !File.Exists(args[0]))
+            {
+                throw new ArgumentException("File is required", nameof(args));
+            }
+
+            string input = await File.ReadAllTextAsync(args[0]);
 
             ICharStream stream = CharStreams.fromString(input);
             ITokenSource lexer = new TodoistGrammarLexer(stream);
             ITokenStream tokens = new CommonTokenStream(lexer);
+            // PrintTokens(tokens, input.Length);
             var parser = new TodoistGrammarParser(tokens);
             parser.RemoveErrorListeners();
             parser.AddErrorListener(new ConsoleErrorListener());
@@ -33,6 +39,25 @@ namespace MarkdownToTodoist.Cli
                 }));
 
             Console.ReadLine();
+        }
+
+        private static void PrintTokens(ITokenStream tokens, int inputLength)
+        {
+            for (int i = 0; i < inputLength; i++)
+            {
+                try
+                {
+                    tokens.Consume();
+                    var token = tokens.Get(i);
+                    var symbolicName = TodoistGrammarLexer.DefaultVocabulary.GetSymbolicName(token.Type);
+                    Console.WriteLine(symbolicName);
+                }
+                catch
+                {
+                    tokens.Seek(0);
+                    break;
+                }
+            }
         }
     }
 }
